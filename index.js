aabed29d959baeffedac10028651f97cc2afcf1a
pinetechnician <pilotaadam@gmail.com>
2023-12-15 11:43:13 -0500

    Addition of CSS animation to fade in banner on load.  Also, a minor tweak to JS code to see if it fixes an extremely minor thing bothering me with the mobile navigation menu...

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const myFunction = () => {
     let x = document.getElementById("nav");
+    let y = document.getElementsByClassName("icon");
     if (x.style.display === "block") {
         x.style.display = "none";
+        y.style.color = "black";
     } else {
         x.style.display = "block";
     }
@@ -11,7 +13,7 @@ const mql = window.matchMedia("(min-width: 1025px)");
 
 mql.onchange = (e) => {
   let x = document.getElementById("nav");
-  let y = document.querySelector("icon")  
+  let y = document.querySelector("icon");  
   if (e.matches) {
     /* the viewport is 600 pixels wide or less */
     x.style.display = "flex";
