18de53c50ba29a16be223267774da55e1f288aae
pinetechnician <pilotaadam@gmail.com>
2023-12-14 19:30:59 -0500

    fix for the bug affecting resizing when the hamburger menu is deployed

--- index.js
+++ index.js
@@ -6,3 +6,17 @@ const myFunction = () => {
         x.style.display = "block";
     }
 }
+
+const mql = window.matchMedia("(min-width: 1025px)");
+
+mql.onchange = (e) => {
+  let x = document.getElementById("nav");
+  let y = document.querySelector("icon")  
+  if (e.matches) {
+    /* the viewport is 600 pixels wide or less */
+    x.style.display = "flex";
+    y.style.display = "none";
+  } else {
+    x.style.display = "none";
+  }
+};
